83c1934f309d8b96230d50fbdf0eb128f6bbb571
SW-Muriu <sw.muriu@gmail.com>
2023-10-09 12:33:47 +0300

    Configurations of the applications layout still in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { LoaderComponent } from './layout/loader/loader.component';
+import { SearchBarComponent } from './layout/search-bar/search-bar.component';
+import { NotFoundComponent } from './layout/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoaderComponent,
+    SearchBarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
